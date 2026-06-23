cbaeaa7e18d0a652267f4d7faaca41d4bc9d01cf
oevinqu <130212677+oevinqu@users.noreply.github.com>
2026-06-23 16:14:54 +0900

    chore: localStack Timeout 세팅값 배포와 통일 (#204)

--- localstack/init/ready.d/create-queues.sh
+++ localstack/init/ready.d/create-queues.sh
@@ -5,4 +5,7 @@ DLQ_ARN=$(awslocal sqs get-queue-attributes \
   --queue-url "$(awslocal sqs get-queue-url --queue-name tripkey-enrichment-dlq --query QueueUrl --output text)" \
   --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
 awslocal sqs create-queue --queue-name tripkey-enrichment \
-  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"${DLQ_ARN}\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"}"
+  --attributes "{
+  \"VisibilityTimeout\":\"180\",
+  \"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"${DLQ_ARN}\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"
+  }"
